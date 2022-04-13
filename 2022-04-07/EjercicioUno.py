3a6a16e879024a25823bfbfce6807b16dacd01fa
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-04-12 20:20:11 -0400

    Declaración de variables y funciones matemáticas

---
+++ 2022-04-07/EjercicioUno.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 20:09:15 2022
+
+@author: Rubenz
+"""
+
+# valor_uno = 3
+# print(valor_uno)
+
+# # valor = 'Valor'
+# # print(valor)
+
+# valor_dos = 3.0
+# print(valor_dos)
+
+# valor_multiplicacion = valor_uno*valor_dos
+# print(valor_multiplicacion)
+
+
+
+# valor_uno = (int)(input('Ingrese el valor uno: '))
+#print(valor_uno)
+
+# # valor = 'Valor'
+# # print(valor)
+
+# valor_dos = (float)(input('Ingrese el valor dos: '))
+# print(valor_dos)
+
+# valor_multiplicacion = valor_uno*valor_dos
+# print(valor_multiplicacion)
+
+
+import math
+
+
+raiz = math.sqrt(16)
+print(raiz)
+
+#True equivale a 1
+raiz = math.sqrt(True)
+print(raiz)
+
+#False equivale a 0
+raiz = math.sqrt(False)
+print(raiz)
+
+#Genera un error, porque no permite numeros negativos
+raiz = math.sqrt(-16)
+print(raiz)
+

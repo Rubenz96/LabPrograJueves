de626184d0724df26b820e2e225fec973150c3cd
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-04-19 21:39:23 -0400

    Create EjercicioUno.py

---
+++ 2022-04-14/EjercicioUno.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 19:38:12 2022
+
+@author: Rubenz
+"""
+
+
+
+# valor_uno = (int)(input('Ingrese el valor uno: '))
+# valor_dos = (int)(input('Ingrese el valor dos: '))
+
+# if(valor_uno > valor_dos):
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+#   print('El valor uno es mayor')
+# else:
+#   print('El valor uno no es mayor')
+#   print('El valor uno no es mayor')
+#   print('El valor uno no es mayor')
+#   print('El valor uno no es mayor')
+#   print('El valor uno no es mayor')
+  
+
+# valor = False
+
+# if(valor):
+#   print('Entró en bloque if')
+# else:
+#   print('No entró en bloque if')
+
+#######################################################
+
+# condicion = 5 < 2
+# print(condicion)
+
+# if(condicion):
+#   print('Entró en bloque if')
+# else:
+#   print('No entró en bloque if')
+
+#######################################################
+
+# variable = None # 0,1,'Hola'
+# print(variable)
+
+# if(variable):
+#   print('Entró en bloque if')
+# else:
+#   print('No entró en bloque if')
+
+# valor = (int)(input('Ingresa un valor: '))
+
+# if(valor > 0):
+#   print('El valor ', valor ,'es mayor a cero')
+# else:
+#   print('El valor ', valor ,'no es mayor a cero')
+  
+
+# if(valor >= 5):
+#   print('El valor ', valor ,'es mayor o igual a cinco')
+# else:
+#   print('El valor ', valor ,'no es mayor a cinco')
+
+
+
+valor = (int)(input('Ingresa un valor: '))
+
+if(valor > 0):
+  print('El valor ', valor ,'es mayor a cero')
+  if(valor <= 5):
+    print('El valor ', valor ,'es mayor a cero y menor o igual a cinco')
+  else:
+    print('El valor ', valor ,'es mayor a cero y mayor a cinco')
+else:
+  print('El valor ', valor ,'no es mayor a cero')
+
+
+
+# 4 alumnos
+# 2 notas
+# Promedio
+# Cual es el que tiene el mejor promedio de los 4
+# Ocupar inputs
+# Notas float
+# No ocupar and u or
+# No ocupar elif
+  
+alumno1_nota1 = (float)(input('Ingrese nota'))
+alumno1_nota2 = (float)(input('Ingrese nota'))
+
+alumno_nota1 = (float)(input('Ingrese nota'))
+alumno_nota2 = (float)(input('Ingrese nota'))
+
+
+

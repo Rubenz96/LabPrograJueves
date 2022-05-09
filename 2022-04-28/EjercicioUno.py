71578a878eac1b92a0a918110781d23251dc044a
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-05-09 19:44:27 -0400

    Clases 28/04 y 05/05

---
+++ 2022-04-28/EjercicioUno.py
@@ -0,0 +1,32 @@
+
+
+# cantidad = (int)(input('Ingrese la cantidad de iteraciones: '))
+# for i in range(cantidad):
+#   print(i)
+
+##############################################
+
+# cantidad = (int)(input('Ingrese la cantidad de iteraciones: '))
+# for i in range(5,cantidad):
+#   print(i)
+###############################################
+# sumatoria = 0
+# cantidad_notas = (int)(input('Ingrese la cantidad de notas: '))
+
+# for i in range(cantidad_notas):
+#   nota = (float)(input('Ingrese la nota: '))
+#   sumatoria = sumatoria + nota
+
+# promedio = sumatoria/cantidad_notas
+# print(promedio)
+
+###############################################
+
+sumatoria = 0
+cantidad = (int)(input('Ingrese valor n de la sumatoria: '))
+
+for n in range(cantidad+1):
+  print(n)
+  sumatoria = sumatoria + n
+
+print(sumatoria)
---
+++ 2022-05-05/EjercicioUno.py
@@ -0,0 +1,62 @@
+
+# #1 si quiere que ejecute ; 2 Si no quiere
+# opcion = (int)(input('Ingrese la opción: '))
+
+# while(opcion != 2):
+#   opcion = (int)(input('Ingrese la opción: '))
+#   print('La opción es: ',opcion)
+
+#############################################
+
+# condicion = 15 < 100
+# print(condicion)
+
+# while(condicion):
+#   print(condicion)
+
+#############################################
+
+# cantidad_notas = (int)(input('Ingrese la cantidad de notas: '))
+# sumador = 0
+# contador = 0
+
+# while(contador<cantidad_notas):
+
+#   nota = (float)(input('Ingrese una nota: '))
+#   sumador = sumador + nota
+#   contador = contador +1
+  
+# promedio = sumador / cantidad_notas
+# print(promedio)
+
+#######################################
+#Sumen los numeros del 1-5000
+
+cantidad = (int)(input('Ingrese la cantidad: '))
+sumador = 0
+contador = 0
+
+while(contador<=cantidad):
+  sumador = sumador + contador
+  contador = contador + 1 
+  
+print(sumador)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
